7fac3ffcdcd5f9b2ba85857e73c880a818d3f21b
peter-sp-1 <samuelpeteropeyemi@gmail.com>
2024-10-14 22:59:14 +0100

    git fix

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Sect1 from "./components/Sect1";
+
+function App () {
+  return (
+    <Sect1 />
+    // <>
+    
+    //   <div className='min-h-screen text-2xl text-black flex flex-col items-center'>
+    //     <p>HAPPY ARE YE!</p>
+    //     <Sect1 />
+    //   </div>
+    // </>
+    
+  );
+};
+
+export default App;
---
+++ src/components/Sect1.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Sect1: React.FC = () => {
+    return (
+        <div className="flex h-[400px] w-full bg-green text-black">SECT1</div>
+    );
+};
+
+export default Sect1;
